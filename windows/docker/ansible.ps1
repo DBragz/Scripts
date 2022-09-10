190e8358a3cafdb033bc1659de0b3c496f1e1b0d
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-09-10 03:12:06 -0400

    Renamed file and fixed links.

--- windows/docker/ansible.ps1
+++ windows/docker/ansible.ps1
@@ -5,7 +5,7 @@
 .NOTES
 	Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
 .LINK
-	https://github.com/DBragz/Scripts/blob/main/windows/powershell/docker/ansible.ps1
+	https://github.com/DBragz/Scripts/blob/main/windows/docker/ansible.ps1
 #>
 
 docker run --rm -it -v Get-Location:/ansible willhallonline/ansible:latest /bin/sh
