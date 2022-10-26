e481354f27cb0aac2ed515f0275633f0e7a8fc0a
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2022-10-25 22:26:33 -0400

    Added group script.

---
+++ cat/group.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Script Name: group.sh
+#
+# Author: Daniel Ribeirinha-Braga - dmrbraga@gmail.com
+#
+# Description: Retrieves the hardware information of the unix environment.
+#
+
+cat /etc/group
