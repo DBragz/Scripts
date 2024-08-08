6363061fbf918bbe4b04a86fe7f4330efc797e56
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-08-07 22:38:16 -0400

    Moved user environment script.

---
+++ windows/powershell/user_env.ps1
@@ -0,0 +1,16 @@
+<# .SYNOPSIS
+	Set users environment variables
+.DESCRIPTION
+	Script for setting up a user environment variable.
+.NOTES
+  Name:   user_env.ps1
+	Author: Daniel Ribeirinha-Braga
+#>
+
+if ((-not $args[0]) -or (-not $args[1])) {
+  return Write-Host "Error: Parameters missing`nUsage: user_env.ps1 [<env_name>] [<env_value>]"
+}
+
+[Environment]::SetEnvironmentVariable(($args[0]), ($args[1]), [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable(($args[0]), ($args[1]), [System.EnvironmentVariableTarget]::Process)
+
