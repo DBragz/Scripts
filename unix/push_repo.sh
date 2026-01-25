3d193dfa55e30f4ec8c473046c4079829092423f
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2026-01-25 03:01:08 +0000

    Created push_repo.sh

---
+++ unix/push_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Name: push_repo.sh
+#
+# Author: Daniel Ribeirinha-Braga
+#
+# Description: A script that uses environment variables to setup your GitHub crednetials and pushes the repository that is the parent of this repositories directory to another GitHub repo.
+#
+
+cat /etc/os-release
+if [ -z $1 ] && [ -z $2 ]; then
+  echo -e "Error: Parameters missing\nUsage: push_repo.sh [environment_variable] [repository]"
+  exit 0
+fi
+
+echo $1
+echo $2
