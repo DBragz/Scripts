03e833d992d87c1f17ab30e320f685949d3ece49
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2026-01-25 06:26:46 +0000

    Changing name of scriot.

--- unix/token_access.sh
+++ unix/token_access.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
-# Name: push_repo.sh
+# Name: token_access.sh
 #
 # Author: Daniel Ribeirinha-Braga
 #
-# Description: A script that uses environment variables to setup your GitHub crednetials and pushes the repository that is the parent of this repositories directory to another GitHub repo.
+# Description: A script that uses environment variables to setup your GitHub crednetials with a token.
 #
 
 if [ -z $1 ] && [ -z $2 ] && [ -z $3 ] && [ -z $4 ]; then
-  echo -e "Error: Parameters missing\nUsage: push_repo.sh [environment_variable] [repository] [git_name] [git_email]"
+  echo -e "Error: Parameters missing\nUsage: token_access.sh [environment_variable] [repository] [git_name] [git_email]"
   exit 0
 fi
 
