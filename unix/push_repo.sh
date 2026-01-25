d473317cefbb0c3257472c717a91944e78d91281
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2026-01-25 03:03:33 +0000

    Removed old command.

--- unix/push_repo.sh
+++ unix/push_repo.sh
@@ -7,7 +7,6 @@
 # Description: A script that uses environment variables to setup your GitHub crednetials and pushes the repository that is the parent of this repositories directory to another GitHub repo.
 #
 
-cat /etc/os-release
 if [ -z $1 ] && [ -z $2 ]; then
   echo -e "Error: Parameters missing\nUsage: push_repo.sh [environment_variable] [repository]"
   exit 0
