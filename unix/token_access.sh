05b8fd1e3fbb1272d2415753249f1ebea62ad1a4
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2026-01-25 06:42:39 +0000

    Added username to script and .gitignore file.

--- unix/token_access.sh
+++ unix/token_access.sh
@@ -7,23 +7,27 @@
 # Description: A script that uses environment variables to setup your GitHub crednetials with a token.
 #
 
-if [ -z $1 ] && [ -z $2 ] && [ -z $3 ] && [ -z $4 ]; then
-  echo -e "Error: Parameters missing\nUsage: token_access.sh [environment_variable] [repository] [git_name] [git_email]"
+if [ -z $1 ] && [ -z $2 ] && [ -z $3 ]; then
+  echo -e "Error: Parameters missing\nUsage: token_access.sh [environment_variable] [repository] [username]"
   exit 0
 fi
 
 SECRET=$(printenv "$1")
 REPO=$2
-GIT_NAME=$3
-GIT_EMAIL=$4
-LOCAL_GIT_NAME=$(git config --get user.name)
-LOCAL_GIT_EMAIL=$(git config --get user.email)
+USERNAME=$3
+# GIT_NAME=$3
+# GIT_EMAIL=$4
+# LOCAL_GIT_NAME=$(git config --get user.name)
+# LOCAL_GIT_EMAIL=$(git config --get user.email)
 
 echo "Secret: $SECRET"
 echo "Repo: $REPO"
-echo "Git Name: $GIT_NAME"
-echo "Git Email: $GIT_EMAIL"
-echo "Local Git Name: $LOCAL_GIT_NAME"
-echo "Local Git Email: $LOCAL_GIT_EMAIL"
+echo "Username: $USERNAME"
+# echo "Git Name: $GIT_NAME"
+# echo "Git Email: $GIT_EMAIL"
+# echo "Local Git Name: $LOCAL_GIT_NAME"
+# echo "Local Git Email: $LOCAL_GIT_EMAIL"
 
 # ./unix/git_init.sh $GIT_NAME $GIT_EMAIL
+
+# git remote add origin https://$GIT_USER:$SECRET@github.com/$GIT_USER/$REPO.git
